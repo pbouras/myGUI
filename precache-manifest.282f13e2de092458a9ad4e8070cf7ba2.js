ec6e32b3c494bf875aa8f3cd78cebcbd98973e2a
pbouras <panagiotis_bouras@hotmail.com>
2020-12-02 15:49:59 +0200

    Updates

---
+++ precache-manifest.282f13e2de092458a9ad4e8070cf7ba2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ea5833a2b90c7473b7477182f6d7d2",
+    "url": "/myGUI/index.html"
+  },
+  {
+    "revision": "8aa53c667b1c72b2ac75",
+    "url": "/myGUI/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "28bb61d064bc7a057359",
+    "url": "/myGUI/static/css/main.4a14cb8e.chunk.css"
+  },
+  {
+    "revision": "8aa53c667b1c72b2ac75",
+    "url": "/myGUI/static/js/2.1ac0b576.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/myGUI/static/js/2.1ac0b576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28bb61d064bc7a057359",
+    "url": "/myGUI/static/js/main.6d4f6f3c.chunk.js"
+  },
+  {
+    "revision": "f130aeea2ea71cfa9cd0",
+    "url": "/myGUI/static/js/runtime-main.7c8d892d.js"
+  }
+]);
